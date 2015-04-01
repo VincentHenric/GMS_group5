7a22f1da39daee16f84bdabaf769467b64fcd5fe
brunojacquelin <jacquelin.br@gmail.com>
2015-04-01 10:31:27 +0800

    tool for easy Folder+Batsh+Atom opening

---
+++ Team Info & Git Tools/Git Tools/GitBash.bat
@@ -0,0 +1,14 @@
+cd Tsinghua/Spring Semester/GMS/GMS_group5
+start .
+start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login -i
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd AppData\Local\atom\bin
+atom.cmd
+
+
+
+
